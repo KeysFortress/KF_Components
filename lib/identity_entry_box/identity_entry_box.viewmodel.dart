d46ea26fbf457da1b7c0325c882568cc09a2dc59
Kristifor <km@aizen.io>
2024-03-02 18:20:05 +0200

    Assigning id to every identity upon creation.

--- lib/identity_entry_box/identity_entry_box.viewmodel.dart
+++ lib/identity_entry_box/identity_entry_box.viewmodel.dart
@@ -1,16 +1,19 @@
 import 'package:domain/models/key_dto.dart';
 import 'package:domain/models/stored_identity.dart';
 import 'package:infrastructure/interfaces/iidentity_manager.dart';
+import 'package:infrastructure/interfaces/ilocal_storage.dart';
 import 'package:shared/component_base_model.dart';
 
 class IdentityEntryBoxViewModel extends ComponentBaseModel {
   late IIdentityManager _identityManager;
+  late IlocalStorage _storage;
   final KeyDto keyData;
   late String _name;
   late String _email;
 
   IdentityEntryBoxViewModel(super.context, this.keyData) {
     _identityManager = getIt.get<IIdentityManager>();
+    _storage = getIt.get<IlocalStorage>();
   }
 
   onNameChanged(String name) {
@@ -18,8 +21,11 @@ class IdentityEntryBoxViewModel extends ComponentBaseModel {
   }
 
   Future onSave() async {
+    var id = await _storage.generateId();
+
     var result = await _identityManager.setSecret(
       StoredIdentity(
+        id,
         _name,
         keyData.publicKey,
         keyData.privateKey,
