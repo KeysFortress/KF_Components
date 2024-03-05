b8fdfa6f99636f18971e4ed79a1e966943e492d4
Kristifor Milchev <km@airzen.io>
2024-03-05 16:07:30 +0200

    Ensure to the router is unlocked after filling otpt code.

--- lib/fill_totp_code/fill_totp_code_viewmodel.dart
+++ lib/fill_totp_code/fill_totp_code_viewmodel.dart
@@ -1,12 +1,16 @@
 import 'package:domain/models/enums.dart';
 import 'package:domain/models/transition_data.dart';
 import 'package:infrastructure/interfaces/iauthorization_service.dart';
+import 'package:infrastructure/interfaces/ipage_router_service.dart';
 import 'package:shared/page_view_model.dart';
 
 class FillTotpCodeViewModel extends PageViewModel {
+  late IAuthorizationService _authorizationService;
+  late IPageRouterService _routerService;
+
   bool _isLocked = true;
   bool get isLocked => _isLocked;
-  late IAuthorizationService _authorizationService;
+
   FillTotpCodeViewModel(super.context) {
     _authorizationService = getIt.get<IAuthorizationService>();
     _isLocked = true;
@@ -22,6 +26,7 @@ class FillTotpCodeViewModel extends PageViewModel {
 
     var match = await _authorizationService.unlockTotp(code);
     if (!match) return;
+    _routerService.isLocked = false;
 
     _isLocked = false;
     notifyListeners();
