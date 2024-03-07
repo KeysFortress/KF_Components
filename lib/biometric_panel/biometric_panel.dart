ca137c988bb4439b16c26076e8cfc1b43580c7d1
Kristifor Milchev <km@airzen.io>
2024-03-07 16:50:57 +0200

    Adding styles to the fail to authenticate replacement.

--- lib/biometric_panel/biometric_panel.dart
+++ lib/biometric_panel/biometric_panel.dart
@@ -1,5 +1,6 @@
 import 'package:components/authentication_failed/authentication_failed.dart';
 import 'package:components/biometric_panel/biometric_panel_viewmodel.dart';
+import 'package:domain/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
@@ -10,8 +11,19 @@ class BiometricPanel extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder.reactive(
       viewModelBuilder: () => BiometricPanelViewModel(context),
+      onViewModelReady: (viewModel) => viewModel.ready(),
       builder: (context, viewModel, child) => viewModel.failedToAuthenticate
-          ? AuthenticationFailed(onTryAgain: viewModel.onTryAgain)
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: ThemeStyles.height! / 3,
+                  child: AuthenticationFailed(
+                    onTryAgain: viewModel.onTryAgain,
+                  ),
+                ),
+              ],
+            )
           : const Column(
               children: [],
             ),
