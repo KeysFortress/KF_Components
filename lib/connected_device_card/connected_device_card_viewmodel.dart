07482822c8f7d4ac5fb9ebbc6d17e4dbfbf801e5
Kristifor Milchev <km@airzen.io>
2024-03-08 17:50:09 +0200

    Adding data bindings for the device connection type in the device list view.

--- lib/connected_device_card/connected_device_card_viewmodel.dart
+++ lib/connected_device_card/connected_device_card_viewmodel.dart
@@ -1,22 +1,29 @@
 import 'package:domain/models/device.dart';
+import 'package:domain/models/enums.dart';
 import 'package:infrastructure/interfaces/idevices_service.dart';
+import 'package:infrastructure/interfaces/isync_service.dart';
 import 'package:shared/component_base_model.dart';
 
 class ConnectedDeviceCardViewModel extends ComponentBaseModel {
   late IDevicesService _devicesService;
+  late ISyncService _syncService;
 
   late Device _device;
   Device get device => _device;
   bool _isOnline = false;
   bool get isOnline => _isOnline;
+  SyncTypes _activeType = SyncTypes.otc;
+  SyncTypes get activeType => _activeType;
 
   ConnectedDeviceCardViewModel(super.context, Device currentDevice) {
     _device = currentDevice;
     _devicesService = getIt.get<IDevicesService>();
+    _syncService = getIt.get<ISyncService>();
   }
 
   ready() async {
     _isOnline = await _devicesService.isDeviceConnected(_device);
+    _activeType = await _syncService.getSyncType(_device.mac);
     notifyListeners();
   }
 }
