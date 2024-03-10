4cb90fa4c0c6581c2277e2987172fcdffbc68b8f
Kristifor Milchev <km@airzen.io>
2024-03-10 16:02:22 +0200

    making the copy optional, adding a checkbox (also opitonal)

--- lib/secret_card/secret_card.dart
+++ lib/secret_card/secret_card.dart
@@ -9,7 +9,19 @@ import 'package:stacked/stacked.dart';
 
 class SecretCard extends StatelessWidget {
   final StoredSecret secret;
-  const SecretCard({super.key, required this.secret});
+  final bool copyEnabled;
+  final bool checkboxEnabled;
+  final bool isChecked;
+  final Function? onActionCallback;
+
+  const SecretCard({
+    super.key,
+    required this.secret,
+    this.copyEnabled = true,
+    this.checkboxEnabled = false,
+    this.isChecked = false,
+    this.onActionCallback,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -22,79 +34,101 @@ class SecretCard extends StatelessWidget {
           color: ThemeStyles.theme.background200,
           borderRadius: BorderRadius.circular(4),
         ),
-        child: CustomButton(
-          callback: viewModel.onCopyPressed,
-          widget: Stack(
-            children: [
-              Positioned(
-                right: 0,
-                child: CustomButton(
-                  widget: SvgPicture.asset(
-                    "assets/images/copy.svg",
-                    package: 'domain',
-                    width: 20,
-                    height: 20,
-                  ),
-                  callback: () {},
-                ),
-              ),
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SvgPicture.asset(
-                        'assets/images/website-password.svg',
+        child: Builder(
+          builder: (context) {
+            var pageData = Stack(
+              children: [
+                if (copyEnabled)
+                  Positioned(
+                    right: 0,
+                    child: CustomButton(
+                      widget: SvgPicture.asset(
+                        "assets/images/copy.svg",
                         package: 'domain',
-                        width: 40,
-                        height: 40,
-                        colorFilter: ColorFilter.mode(
-                          ThemeStyles.theme.primary300,
-                          BlendMode.srcIn,
-                        ),
+                        width: 20,
+                        height: 20,
                       ),
-                      Expanded(
-                        flex: 1,
-                        child: Text(
-                          secret.name,
-                          style: ThemeStyles.regularParagraphOv(
-                            size: 16,
-                            color: ThemeStyles.theme.primary300,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      )
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: PasswordStrenght(
-                      initial: secret.content,
+                      callback: () {},
                     ),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        secret.username,
-                        style: ThemeStyles.regularParagraphOv(
-                          size: 12,
-                          color: ThemeStyles.theme.primary300,
+                if (checkboxEnabled)
+                  Positioned(
+                    right: 0,
+                    child: Checkbox(
+                      activeColor: ThemeStyles.theme.primary300,
+                      value: isChecked,
+                      onChanged: (value) => () {},
+                    ),
+                  ),
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SvgPicture.asset(
+                          'assets/images/website-password.svg',
+                          package: 'domain',
+                          width: 40,
+                          height: 40,
+                          colorFilter: ColorFilter.mode(
+                            ThemeStyles.theme.primary300,
+                            BlendMode.srcIn,
+                          ),
                         ),
+                        Expanded(
+                          flex: 1,
+                          child: Text(
+                            secret.name,
+                            style: ThemeStyles.regularParagraphOv(
+                              size: 16,
+                              color: ThemeStyles.theme.primary300,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: PasswordStrenght(
+                        initial: secret.content,
                       ),
-                      Text(
-                        "Last used: ${secret.lastUsed.day}/${secret.lastUsed.month}/${secret.lastUsed.year}",
-                        style: ThemeStyles.regularParagraphOv(
-                          size: 12,
-                          color: ThemeStyles.theme.primary300,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          secret.username,
+                          style: ThemeStyles.regularParagraphOv(
+                            size: 12,
+                            color: ThemeStyles.theme.primary300,
+                          ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ],
-          ),
+                        Text(
+                          "Last used: ${secret.lastUsed.day}/${secret.lastUsed.month}/${secret.lastUsed.year}",
+                          style: ThemeStyles.regularParagraphOv(
+                            size: 12,
+                            color: ThemeStyles.theme.primary300,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            );
+
+            if (copyEnabled) {
+              return CustomButton(
+                callback: copyEnabled
+                    ? viewModel.onCopyPressed
+                    : () => onActionCallback?.call(),
+                widget: pageData,
+              );
+            }
+
+            return pageData;
+          },
         ),
       ),
     );
